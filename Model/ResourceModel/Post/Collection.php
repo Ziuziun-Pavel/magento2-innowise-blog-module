e342e98c976cda0bc484f61fddc3eb4d3cfb822f
Ziuziun Pavel <zuzunpavel@gmail.com>
2023-01-06 11:39:20 +0100

    Add CRUD-operations for Posts

---
+++ Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Innowise\Blog\Model\ResourceModel\Post;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Innowise\Blog\Model\Post;
+use Innowise\Blog\Model\ResourceModel\Post as PostResource;
+
+class Collection extends AbstractCollection
+{
+    protected function _construct()
+    {
+        $this->_init(Post::class, PostResource::class);
+    }
+}
